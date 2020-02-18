ff2ebce6bf2074eccd30126c65465f9b956af1db
John Zhang <jnzhang622@gmail.com>
2020-02-18 23:17:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,6 @@ def roll_call_dwarves(dwarves)an argument here
   # Your code here
   index = 0
   dwarves.each {|dwarf| puts /{index + 1} {dwarves[index]}/}
-  end
-    
 end
 
 def summon_captain_planet# code an argument here
