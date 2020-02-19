ec319959809f7bb56ee3e41888f9a20e5e8a9976
John Zhang <jnzhang622@gmail.com>
2020-02-19 00:03:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def roll_call_dwarves(arr)
     end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arg)# code an argument here
   # Your code here
+  arg.length
 end
 
 def long_planeteer_calls# code an argument here
