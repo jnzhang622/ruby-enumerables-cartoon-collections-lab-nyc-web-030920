4a944c9b53d33c9ce5f97ad33cab9c1e2b339938
John Zhang <jnzhang622@gmail.com>
2020-02-19 00:04:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ end
 
 def summon_captain_planet(arg)# code an argument here
   # Your code here
-  return arg.size
+  result =  arg.size
+  puts result
 end
 
 def long_planeteer_calls# code an argument here
