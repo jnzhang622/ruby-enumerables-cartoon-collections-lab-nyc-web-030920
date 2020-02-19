b0b6340489050ec632d57b635a5705678a8f7629
John Zhang <jnzhang622@gmail.com>
2020-02-19 00:36:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,21 +6,21 @@ def roll_call_dwarves(dwarves)
     end
 end
 
-def summon_captain_planet(arg)# code an argument here
+def summon_captain_planet(arg)
   # Your code here
   arg = arg.map {|i| i+"!"}
     arg = arg.map {|c| c.capitalize}
    return arg
 end
 
-def long_planeteer_calls(calls)# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
   calls.any? do |call|
     call.length > 4
 end
 end
 
-def find_the_cheese(array)# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
     i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
